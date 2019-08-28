51c171f2254ca89f78dbd8be9f3e3119c6480a7a
Alberto Galdámez <albeerto.galdamezh@gmail.com>
2019-08-27 23:12:48 -0600

    Enviar para verificacion

---
+++ Practica_Num4/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Practica_Num4
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            //Variables y Arreglos
+            decimal[] sueldos;
+            sueldos = new decimal[21];
+            int i;
+
+
+            //Recolección de datos
+            do
+            {
+                for (i = 1; i <= 20; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Escriba el sueldo del empleado Núm.{0}", i);
+                    Console.Write("$ ");
+                    if (decimal.TryParse(Console.ReadLine(), out sueldos[i]))
+                    {
+                        //El programa continua con normalidad.
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("Dato ingresado no valido (No es un sueldo)");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Presione <Enter> para continuar");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.ReadKey();
+                        i = i - 1;
+                    }
+
+                }
+
+            } while (i != 21);
+
+            Console.Clear();
+            Console.WriteLine("----Muestra de Sueldos----");
+            for (int t = 1; t <= 20; t++)
+            {
+                Console.WriteLine("Sueldo del empleado Núm.{0}: ${1}", t, sueldos[t]);
+            }
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("--Se han mostrado todos los sueldos--");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
---
+++ Practica_Num4/Ejercicios/Ejercicio2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Practica_Num4.Ejercicios
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            //Variables y Arreglos
+            int llevaCuentas;
+            decimal[] notas;
+            int[] asistencia;
+
+
+            //Confirmaciones
+            bool C1 = false;
+            bool C2 = false;
+
+            //Proceso: Obtener datos.
+            do
+            {
+                Console.Clear();
+                Console.Write("Sistema de Califcación y Asistencia final: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Programación Estructurada");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Escriba el numero de Alumnos: ");
+                if (int.TryParse(Console.ReadLine(), out llevaCuentas))
+                {
+                    //El programa puede entrar al siguiente proceso.
+
+                    C1 = true;
+                    do
+                    {
+                        for (int i = 0; i <= llevaCuentas - 1; i++)
+                        {
+                            notas = new decimal[llevaCuentas];
+                            Console.Clear();
+                            Console.Write("Escriba la nota del Num.{0}: ", i + 1);
+                            if (decimal.TryParse(Console.ReadLine(), out notas[i]))
+                            {
+
+                                //El programa continua.
+                                Console.Write("Porcentaje de asistencia: % ");
+                                asistencia = new int[llevaCuentas];
+                                if (int.TryParse(Console.ReadLine(), out asistencia[i]))
+                                {
+
+                                    //El programa continua
+                                    C2 = true;
+
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Dato Incorrecto");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.ReadKey();
+                                }
+                            }
+                            else
+                            {
+                                //El programa vuelve a pedir el dato
+
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("----Dato invalido----");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.ReadKey();
+                                i = i - 1;
+                            }
+
+
+                        }
+
+
+                    } while (C2 == false);
+                }
+
+                else
+                {
+
+                    //El programa vuelve a pedir el dato.
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("----Dato invalido-----");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                }
+
+                
+
+            } while (C1 == false);
+        }
+    }
+}
